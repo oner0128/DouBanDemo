b24d3e257f30fbf9c89d169b158f87191c7daea5
oner0128 <oner0128@gmail.com>
2017-05-09 23:16:42 +0800

    fix bug's splashActivity

--- app/src/main/java/com/android/oner0128/doubandemo/view/activity/SplashActivity.java
+++ app/src/main/java/com/android/oner0128/doubandemo/view/activity/SplashActivity.java
@@ -2,6 +2,7 @@ package com.android.oner0128.doubandemo.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,7 +14,13 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this,MainActivity.class));
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },1000);
+
     }
 }
